e8a87be5b3ce850b63105d4fc26546dd5cb74398
guotongning <guotongning@58.com>
2023-03-24 16:33:37 +0800

    [ddz] 新增牌型飞机，未实现

---
+++ src/com/ning/poker/ddz/poker_group/AircraftPokerGroup.java
@@ -0,0 +1,40 @@
+package com.ning.poker.ddz.poker_group;
+
+import com.ning.poker.base.BasePokerGroup;
+import com.ning.poker.base.api.Poker;
+import com.ning.poker.base.api.PokerGroup;
+import com.ning.poker.base.exception.NoComparabilityException;
+import com.ning.poker.ddz.util.PokerUtils;
+
+public class AircraftPokerGroup extends BasePokerGroup {
+
+    public AircraftPokerGroup(Poker[] pokers) {
+        super(pokers);
+    }
+
+    @Override
+    public int compareTo(PokerGroup other) {
+        if (other == null) {
+            return 1;
+        }
+        if (other instanceof BombPokerGroup) {
+            return -1;
+        }
+        if (!(other instanceof AircraftPokerGroup) || show().length != other.show().length) {
+            throw new NoComparabilityException(this, other);
+        }
+        return Integer.compare(this.score(), other.score());
+    }
+
+    @Override
+    public int score() {
+        return show()[0].score();
+    }
+
+    @Override
+    public void sort(Poker[] pokers) {
+        super.sort(pokers);
+        //找到几张单牌的index。然后交换到尾部。
+
+    }
+}
---
+++ src/com/ning/poker/ddz/poker_group/poker_judge/AircraftPokerAdjudicator.java
@@ -0,0 +1,12 @@
+package com.ning.poker.ddz.poker_group.poker_judge;
+
+import com.ning.poker.base.api.Poker;
+import com.ning.poker.base.api.PokerGroupAdjudicator;
+
+public class AircraftPokerAdjudicator implements PokerGroupAdjudicator {
+
+    @Override
+    public boolean judge(Poker[] pokers) {
+        return false;
+    }
+}
